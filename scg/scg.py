2599fa0b539987374c17f253299efe94c2ef11f3
einarsi <2356425+einarsi@users.noreply.github.com>
2019-05-31 10:02:33 +0200

    Add click for command line parsing

--- scg/scg.py
+++ scg/scg.py
@@ -1,11 +1,18 @@
 import os
+import click
 from jinja2 import Environment, FileSystemLoader
 from helpers.config_parser import parse_config
 from helpers.helpers import read_source
 from helpers.version import __version__
 
-def main(config):
-    cfg = parse_config(config).data
+@click.group()
+def main():
+    pass
+
+@main.command()
+@click.argument('config_file')
+def make(config_file):
+    cfg = parse_config(config_file).data
 
     env = Environment(
         loader=FileSystemLoader(searchpath=os.path.join(cfg['path']['root'], cfg['path']['templatepath']))
@@ -33,5 +40,47 @@ def main(config):
                     print(temp.render(values))
                     f.write(temp.render(values))
 
+@main.command()
+@click.argument('config_file')
+def revert(config_file):
+    cfg = parse_config(config_file).data
+    sources = dict()
+    for source in cfg['sources']:
+        s = read_source(source, cfg['path']['root'])
+        sources[source['id']] = s
+
+    master_path = os.path.join(cfg['path']['root'], cfg['templategenerator']['masterpath'])
+    masters = [f for f in os.listdir(master_path) if os.path.isfile(os.path.join(master_path, f))]
+    template_path = os.path.join(cfg['path']['root'], cfg['path']['templatepath'])
+
+    for filename in masters:
+        if 'includeonly' in cfg['templategenerator'] and filename not in cfg['templategenerator']['includeonly']:
+            continue
+        not_found = True
+        for item in cfg['layout']:
+            if filename == item['name']:
+                not_found = False
+                break
+        source = sources[item['source']]
+        source = source[cfg['templategenerator']['master']]
+
+        f = open(os.path.join(master_path, filename), 'r')
+        txt = f.read()
+
+        for key, value in source.items():
+            key = '{{ '+key+' }}'
+            txt = re.sub(value, key, txt)
+        print(txt)
+        f = open(os.path.join(master_path, filename), 'r')
+
+        txt = f.read()
+        f.close()
+        #print(txt)
+
+
+        #f = open(os.path.join(template_path, filename), 'w')
+        #f.write(txt)
+        #f.close()
+
 if __name__ == '__main__':
-    main('johan_sverdrup.yaml')
+    main()
