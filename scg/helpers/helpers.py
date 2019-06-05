233e7b24963a0c0f5b28ba4d48acbaccc59dc55d
einarsi <2356425+einarsi@users.noreply.github.com>
2019-06-05 13:14:17 +0200

    Fix bug with reading removed rows from Excel sheet

--- scg/helpers/helpers.py
+++ scg/helpers/helpers.py
@@ -50,22 +50,19 @@ def read_source(source, root):
         logger.error(f"Unable to read '{source['filename']}' which belongs to source '{source['id']}'")
         sys.exit(1)
 
-    wb = load_workbook(os.path.join(root, source['filename']), read_only=True, data_only=True)
     sheet = wb[source['sheet']]
-    rows = sheet.max_row
     cols = sheet.max_column
-    headers = dict((i, sheet.cell(row=1, column=i).value) for i in range(1, cols+1))
     ret = dict()
-    for i in range(2, rows + 1):
+    for rownum, row in enumerate(sheet, 1):
+        if rownum == 1:
+            continue
+        if all(c.value is None for c in row):
+            break
         temp = dict()
         for j in range(2, cols + 1):
-            temp[sheet.cell(row=1, column=j).value] = str(sheet.cell(row=i, column=j).value)
-            ret[sheet.cell(row=i, column=1).value] = temp
+            temp[sheet.cell(row=1, column=j).value] = str(sheet.cell(row=rownum, column=j).value)
+            ret[sheet.cell(row=rownum, column=1).value] = temp
     return ret
-    #def item(i, j):
-    #    return (sheet.cell(row=1, column=j).value, sheet.cell(row=i, column=j).value)
-    #return (dict(item(i, j) for j in range(1, cols + 1 )) for i in range(2, rows + 1))
-
 
 def get_all_source_data(sources, path):
     res = dict()
