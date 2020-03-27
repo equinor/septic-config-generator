e85e8afaeb3f165f26c4d20cd4b60cebea07eeab
einarsi <2356425+einarsi@users.noreply.github.com>
2020-03-27 15:11:15 +0100

    Add tests

---
+++ tests/test_scg.py
@@ -0,0 +1,17 @@
+import pytest
+from scg import *
+
+
+@pytest.mark.skip()
+def test_make():
+    pass
+
+
+@pytest.mark.skip()
+def test_revert():
+    pass
+
+
+@pytest.mark.skip()
+def test_diff():
+    pass
