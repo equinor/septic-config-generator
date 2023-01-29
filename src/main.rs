ffc59050e906b3e1c4b38fa421dce778a0ddf59d
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-01-29 09:26:09 +0100

    refactor: split excel read into own function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use calamine::{open_workbook_auto, Reader}; // Use open_workbook instead of open_workbook_auto
+use calamine::{open_workbook, Error, Reader, Xlsx};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -27,31 +27,37 @@ struct Layout {
     include: Option<Vec<String>>,
 }
 
+fn read_source(source: &Source) -> Result<Vec<HashMap<String, String>>, Error> {
+    let path = format!("basic example/{}", source.filename);
+    let mut workbook: Xlsx<_> = open_workbook(path)?;
+    let range = workbook
+        .worksheet_range(&source.sheet)
+        .ok_or(Error::Msg("Cannot find sheet"))??;
+
+    let row_headers = range.rows().next().unwrap();
+    let mut data = vec![];
+    for row in range.rows().skip(1) {
+        let mut row_data = HashMap::new();
+        for (header, cell) in row_headers.iter().zip(row.iter()) {
+            row_data.insert(
+                String::from(header.get_string().unwrap()),
+                String::from(cell.get_string().unwrap()),
+            );
+        }
+        data.push(row_data);
+    }
+    Ok(data)
+}
+
 fn main() {
     let cfg: Config = serde_yaml::from_str(include_str!("../basic example/example.yaml")).unwrap();
     let mut all_source_data: HashMap<String, Vec<HashMap<String, String>>> = HashMap::new();
 
-    for source in cfg.sources {
-        let mut workbook = open_workbook_auto(&format!("{}/{}", "basic example", source.filename))
-            .expect("Cannot open file");
-        if let Some(Ok(sheet)) = workbook.worksheet_range(&source.sheet) {
-            let row_headers = sheet.rows().next().unwrap();
-            let mut excel_data = vec![];
-            for row in sheet.rows().skip(1) {
-                let mut row_data = HashMap::new();
-                for (header, cell) in row_headers.iter().zip(row.iter()) {
-                    row_data.insert(
-                        String::from(header.get_string().unwrap()),
-                        String::from(cell.get_string().unwrap()),
-                    );
-                }
-                excel_data.push(row_data);
-            }
-            all_source_data.insert(source.id, excel_data);
-        }
+    for source in &cfg.sources {
+        let source_data = read_source(source).unwrap();
+        all_source_data.insert(source.id.to_string(), source_data);
     }
 
     // println!("{:?}", config);
     println!("{:?}", all_source_data);
-    println!("{:?}", all_source_data["main"]);
 }
