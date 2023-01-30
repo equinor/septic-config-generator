491aa0ef8b16f60df882279d839eebaa8a55989f
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-01-30 10:45:26 +0100

    fix: handle errors when reading excel as source

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,9 @@ use std::process;
 fn read_source(source: &Source) -> Result<Vec<HashMap<String, String>>, Box<dyn Error>> {
     let path = format!("basic example/{}", source.filename);
     let mut workbook: Xlsx<_> = open_workbook(path)?;
-    let range = workbook.worksheet_range(&source.sheet).ok_or_else(|| {
-        format!(
-            "Cannot find sheet '{}' in file '{}'",
-            source.sheet, source.filename
-        )
-    })??; // Rewrite to handle instead of panic on unknown sheet
+    let range = workbook
+        .worksheet_range(&source.sheet)
+        .ok_or_else(|| format!("Cannot find sheet '{}'", source.sheet))??;
 
     let row_headers = range.rows().next().unwrap();
     let data = range
@@ -38,14 +35,17 @@ fn main() {
     let filename = String::from("basic example/example.yaml");
 
     let cfg = Config::new(&filename).unwrap_or_else(|e| {
-        println!("Problem reading {filename}: {}", e);
+        eprintln!("Problem reading '{}': {}", filename, e);
         process::exit(1)
     });
 
     let mut all_source_data: HashMap<String, Vec<HashMap<String, String>>> = HashMap::new();
 
     for source in &cfg.sources {
-        let source_data = read_source(source).unwrap();
+        let source_data = read_source(source).unwrap_or_else(|e| {
+            eprintln!("Problem reading source file '{}': {}", source.filename, e);
+            process::exit(1);
+        });
         all_source_data.insert(source.id.to_string(), source_data);
     }
 
