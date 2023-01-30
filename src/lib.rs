03e8472588d989fea8715f67b1700c681ed28a83
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-01-30 11:03:44 +0100

    chore: move read_source to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+use calamine::{open_workbook, Reader, Xlsx};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
@@ -33,4 +35,31 @@ pub struct Layout {
     pub name: String,
     pub source: Option<String>,
     pub include: Option<Vec<String>>,
+}
+
+pub fn read_source(source: &Source) -> Result<Vec<HashMap<String, String>>, Box<dyn Error>> {
+    let path = format!("basic example/{}", source.filename);
+    let mut workbook: Xlsx<_> = open_workbook(path)?;
+    let range = workbook
+        .worksheet_range(&source.sheet)
+        .ok_or_else(|| format!("Cannot find sheet '{}'", source.sheet))??;
+
+    let row_headers = range.rows().next().unwrap();
+    let data = range
+        .rows()
+        .skip(1)
+        .map(|row| {
+            row_headers
+                .iter()
+                .zip(row.iter())
+                .map(|(header, cell)| {
+                    (
+                        header.get_string().unwrap().to_string(),
+                        cell.get_string().unwrap().to_string(),
+                    )
+                })
+                .collect::<HashMap<String, String>>()
+        })
+        .collect::<Vec<HashMap<String, String>>>();
+    Ok(data)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
-use calamine::{open_workbook, Reader, Xlsx};
-use septic_config_generator::{Config, Source};
+use septic_config_generator::{read_source, Config};
 use std::collections::HashMap;
-use std::error::Error;
 use std::process;
-
-fn read_source(source: &Source) -> Result<Vec<HashMap<String, String>>, Box<dyn Error>> {
-    let path = format!("basic example/{}", source.filename);
-    let mut workbook: Xlsx<_> = open_workbook(path)?;
-    let range = workbook
-        .worksheet_range(&source.sheet)
-        .ok_or_else(|| format!("Cannot find sheet '{}'", source.sheet))??;
-
-    let row_headers = range.rows().next().unwrap();
-    let data = range
-        .rows()
-        .skip(1)
-        .map(|row| {
-            row_headers
-                .iter()
-                .zip(row.iter())
-                .map(|(header, cell)| {
-                    (
-                        header.get_string().unwrap().to_string(),
-                        cell.get_string().unwrap().to_string(),
-                    )
-                })
-                .collect::<HashMap<String, String>>()
-        })
-        .collect::<Vec<HashMap<String, String>>>();
-    Ok(data)
-}
 
 fn main() {
     let filename = String::from("basic example/example.yaml");
