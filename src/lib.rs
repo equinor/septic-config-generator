de96f44a9641aa8656413733c8a0cde5fec60fa6
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-01-30 15:18:53 +0100

    chore: move argument parser into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
+use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
@@ -62,4 +63,38 @@ pub fn read_source(source: &Source) -> Result<Vec<HashMap<String, String>>, Box<
         })
         .collect::<Vec<HashMap<String, String>>>();
     Ok(data)
+}
+
+#[derive(clap::Parser)]
+#[command(version, about, long_about=None)]
+#[command(next_line_help = true)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+pub enum Commands {
+    /// Generate SEPTIC config
+    Make(MakeArgs),
+    /// Show difference between two text files
+    Diff,
+}
+
+#[derive(clap::Parser)]
+pub struct MakeArgs {
+    /// The yaml config file
+    pub config_file: PathBuf,
+    /// Name of output file (overrides config option "outputfile")
+    #[arg(short, long, value_name = "FILE")]
+    pub output: Option<PathBuf>,
+    /// Do not prompt for verification of output file before overwriting original (overrides config option "verifycontent")
+    #[arg(short, long = "no-verify")]
+    pub noverify: bool,
+    /// Only output warnings or errors
+    #[arg(short, long)]
+    pub silent: bool,
+    /// Gllobal variable to use for all templates, also those without specified source. Can be repeated. Global variables overwrite other variables with same name
+    #[arg(short, long)]
+    pub var: Option<Vec<String>>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,9 @@
 use clap::Parser;
 use log::error;
-use septic_config_generator::{read_source, Config};
+use septic_config_generator::{read_source, Cli, Commands, Config};
 use std::collections::HashMap;
-use std::path::PathBuf;
+
 use std::process;
-
-#[derive(clap::Parser)]
-#[command(version, about, long_about=None)]
-#[command(next_line_help = true)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(clap::Subcommand)]
-enum Commands {
-    /// Generate SEPTIC config
-    Make(MakeArgs),
-    /// Show difference between two text files
-    Diff,
-}
-
-#[derive(clap::Parser)]
-struct MakeArgs {
-    /// The yaml config file
-    config_file: PathBuf,
-    /// Name of output file (overrides config option "outputfile")
-    #[arg(short, long, value_name = "FILE")]
-    output: Option<PathBuf>,
-    /// Do not prompt for verification of output file before overwriting original (overrides config option "verifycontent")
-    #[arg(short, long = "no-verify")]
-    noverify: bool,
-    /// Only output warnings or errors
-    #[arg(short, long)]
-    silent: bool,
-    /// Gllobal variable to use for all templates, also those without specified source. Can be repeated. Global variables overwrite other variables with same name
-    #[arg(short, long)]
-    var: Option<Vec<String>>,
-}
 
 fn main() {
     let args = Cli::parse();
