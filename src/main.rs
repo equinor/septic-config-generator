cb5e801ca3d8bdf7bb8d5800cb216b0c299ba5ac
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-01-31 10:40:17 +0100

    fix: use eprintln instead of log for now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use log::error;
 use septic_config_generator::{args, config::Config, datasource};
 use std::collections::HashMap;
 use std::process;
@@ -19,16 +18,15 @@ fn main() {
             let filename = make_args.config_file;
 
             let cfg = Config::new(&filename).unwrap_or_else(|e| {
-                error!("Problem reading '{}': {}", &filename.display(), e);
+                eprintln!("Problem reading '{}': {}", &filename.display(), e);
                 process::exit(1)
             });
-
             let mut all_source_data: HashMap<String, Vec<HashMap<String, String>>> = HashMap::new();
 
             for source in &cfg.sources {
                 let source_data =
                     datasource::read(&source.filename, &source.sheet).unwrap_or_else(|e| {
-                        error!("Problem reading source file '{}': {}", source.filename, e);
+                        eprintln!("Problem reading source file '{}': {}", source.filename, e);
                         process::exit(1);
                     });
                 all_source_data.insert(source.id.to_string(), source_data);
