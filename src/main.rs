f01ad940c4d87dd3a0a34883bc123ca0fbf6fec2
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-02-02 17:54:02 +0100

    refactor: move make command into own function

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 use std::process;
 
-fn add_missing_yaml_extension(filename: &PathBuf) -> PathBuf {
+fn ensure_has_extension(filename: &PathBuf, extension: &str) -> PathBuf {
     let mut file = filename.clone();
     if file.extension().is_none() {
-        file.set_extension("yaml");
+        file.set_extension(extension);
     }
     file
 }
@@ -22,47 +22,44 @@ fn _merge_maps(
     merged
 }
 
+fn cmd_make(cfg_file: &PathBuf, var_list: &Vec<String>) {
+    let cfg_file = ensure_has_extension(&cfg_file, "yaml");
+
+    let globals = var_list
+        .chunks(2)
+        .map(|chunk| (chunk[0].to_string(), chunk[1].to_string()))
+        .collect::<HashMap<String, String>>();
+
+    let cfg = Config::new(&cfg_file).unwrap_or_else(|e| {
+        eprintln!("Problem reading '{}': {}", &cfg_file.display(), e);
+        process::exit(1)
+    });
+
+    let mut all_source_data: HashMap<String, HashMap<String, HashMap<String, DataType>>> =
+        HashMap::new();
+
+    for source in &cfg.sources {
+        let mut path = PathBuf::from(cfg_file.parent().unwrap());
+        path.push(&source.filename);
+
+        let source_data = datasource::read(&path, &source.sheet).unwrap_or_else(|e| {
+            eprintln!("Problem reading source file '{}': {}", path.display(), e);
+            process::exit(1);
+        });
+        all_source_data.insert(source.id.to_string(), source_data);
+    }
+
+    println!("{:?}", all_source_data);
+    println!("{:?}", all_source_data["main"]["D02"]);
+    println!("{:?}", globals);
+}
+
 fn main() {
     let args = args::Cli::parse();
 
     match args.command {
         args::Commands::Make(make_args) => {
-            let var_list = make_args.var.unwrap_or_default();
-
-            let var_map = var_list
-                .chunks(2)
-                .map(|chunk| (chunk[0].to_string(), chunk[1].to_string()))
-                .collect::<HashMap<String, String>>();
-
-            let cfg_file = add_missing_yaml_extension(&make_args.config_file);
-
-            let cfg = Config::new(&cfg_file).unwrap_or_else(|e| {
-                eprintln!("Problem reading '{}': {}", &cfg_file.display(), e);
-                process::exit(1)
-            });
-
-            let mut all_source_data: HashMap<String, HashMap<String, HashMap<String, DataType>>> =
-                HashMap::new();
-
-            for source in &cfg.sources {
-                let mut path = PathBuf::from(cfg_file.parent().unwrap());
-                path.push(&source.filename);
-
-                let source_data = datasource::read(&path, &source.sheet).unwrap_or_else(|e| {
-                    eprintln!("Problem reading source file '{}': {}", path.display(), e);
-                    process::exit(1);
-                });
-                all_source_data.insert(source.id.to_string(), source_data);
-            }
-
-            // println!("{:?}", config);
-            println!("{:?}", all_source_data);
-            println!("{:?}", all_source_data["main"]["D02"]);
-            println!("{:?}", var_map);
-            // println!(
-            //     "{:?}",
-            //     merge_maps(&all_source_data["main"]["D02"], &var_map)
-            // );
+            cmd_make(&make_args.config_file, &make_args.var.unwrap_or_default());
         }
         args::Commands::Diff => todo!(),
     }
