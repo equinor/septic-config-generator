bda6176200bd14d675a6231c5385c33637fddb8a
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-05-05 15:19:37 +0200

    fix: build for non-Windows

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 #[cfg(windows)]
-use winresource::WindowsResource;
-
 fn main() {
+    use winresource::WindowsResource;
+
     let mut res = WindowsResource::new();
     res.set_language(0x0009);
     res.compile().unwrap();
