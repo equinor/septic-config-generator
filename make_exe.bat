18cfbf53de5ab9e4e35811333f2fe447a23f6ec3
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-08-14 13:16:15 +0200

    chore: remove legacy python code (#153)

--- make_exe.bat
+++
@@ -1,8 +0,0 @@
-@setlocal
-@set PATH=%PATH%;C:\Windows\System32\downlevel;
-venv\Scripts\pyinstaller.exe ^
-  --hiddenimport jinja2_git ^
-  --hiddenimport jinja2_time ^
-  --onefile ^
-  --name scg.exe ^
-  cli.py
