e7b868e3373b0d0dcf34cb6015eb1fee91e17992
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2023-10-10 08:21:21 +0200

    feat: add hidden --token argument, simplify target
    CI changed targets from "windows", "linux" etc to proper target strings.
    --token arg added for testing purposes .

--- src/commands/update.rs
+++ src/commands/update.rs
@@ -2,65 +2,39 @@ use clap::Parser;
 use self_update::cargo_crate_version;
 
 #[derive(Parser, Debug)]
-pub struct Update;
+pub struct Update {
+    /// Provide authentication token (GitHub PAT). Used for testing.
+    #[arg(short, long, hide = true)]
+    pub token: Option<String>,
+}
 
 impl Update {
     pub fn execute(&self) {
-        cmd_update();
+        cmd_update(&self.token);
     }
 }
 
-fn get_target() -> Result<String, String> {
-    let target = self_update::get_target();
-    let supported_targets = ["windows", "darwin", "linux"];
-
-    for supported_target in &supported_targets {
-        if target.contains(supported_target) {
-            return Ok(String::from(*supported_target));
-        }
-    }
-
-    Err(format!("Unsupported target: {}", target))
-}
-
-fn cmd_update() {
-    let target = get_target().unwrap_or_else(|e| {
-        eprintln!("{}", e);
-        std::process::exit(1)
-    });
-
-    self_update::backends::github::Update::configure()
+fn cmd_update(token: &Option<String>) {
+    let binding = self_update::backends::github::Update::configure();
+    let mut updater = binding;
+    let mut updater = updater
         .repo_owner("equinor")
         .repo_name("septic-config-generator")
         .bin_name("scg")
         .show_download_progress(true)
-        .target(&target)
-        .current_version(cargo_crate_version!())
-        .build()
-        .unwrap_or_else(|e| {
-            eprintln!("Problem creating updater: {e}");
-            std::process::exit(1);
-        })
-        .update()
-        .unwrap_or_else(|e| {
-            eprintln!("{e}");
-            std::process::exit(1);
-        });
-}
+        .current_version(cargo_crate_version!());
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn check_get_target_returns_value() {
-        let target = get_target().unwrap();
-        if cfg!(windows) {
-            assert_eq!(target, "windows");
-        } else if cfg!(target_os = "macos") {
-            assert_eq!(target, "darwin");
-        } else if cfg!(target_os = "linux") {
-            assert_eq!(target, "linux");
-        }
+    if let Some(token) = token {
+        updater = updater.auth_token(token)
     }
+
+    let updater = updater.build().unwrap_or_else(|e| {
+        eprintln!("Problem creating updater: {e}");
+        std::process::exit(1);
+    });
+
+    updater.update().unwrap_or_else(|e| {
+        eprintln!("{e}");
+        std::process::exit(1);
+    });
 }
